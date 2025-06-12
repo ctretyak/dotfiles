90499cdb9bfd7ca28e0bc6ece3969b039abe2ba4
ctretyak <ctretyak@gmail.com>
2025-06-12 22:13:08 +0300

    fix: update ZSH_TMUX_AUTOSTART logic for vscode compatibility

--- .chezmoitemplates/linux/zsh/env.zsh
+++ .chezmoitemplates/linux/zsh/env.zsh
@@ -5,6 +5,10 @@ export PATH=$HOME/bin:$HOME/.local/bin:/usr/local/bin:/snap/bin:$PATH
 export GPG_TTY=$TTY
 export EDITOR='nvim'
 export BROWSER='brave'
-export ZSH_TMUX_AUTOSTART=true
+if [[ "$TERM_PROGRAM" == "vscode" ]]; then
+  export ZSH_TMUX_AUTOSTART=false
+else
+  export ZSH_TMUX_AUTOSTART=true
+fi
 export ZSH_TMUX_AUTOCONNECT=true
 export LC_ALL=en_US.UTF-8 # needs for tmux by ssh
