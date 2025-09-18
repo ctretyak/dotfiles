02acf8efacd59369ee323696eee7e87f7fdc4878
ctretyak <ctretyak@gmail.com>
2025-09-18 11:30:10 +0300

    chore: remove obsolete backlight and battery detection scripts, along with several cron job configurations from Ansible playbook

--- .chezmoitemplates/linux/backlight-devices.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-ls /sys/class/backlight 2>/dev/null | head -n 1 || true
--- .chezmoitemplates/linux/has-battery.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if ls /sys/class/power_supply/ | grep -q '^BAT'; then
-  echo true
-else
-  echo false
-fi
