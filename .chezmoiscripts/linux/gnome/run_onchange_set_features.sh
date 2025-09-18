31fd13d20be20d4296fff75ed0fb3d8a832592af
ctretyak <ctretyak@gmail.com>
2025-09-18 11:24:14 +0300

    chore: remove obsolete configuration files and scripts for various applications, including Brave, RDP, and screenshot tools, and update GNOME extension settings

--- .chezmoiscripts/linux/gnome/run_onchange_set_features.sh
+++ .chezmoiscripts/linux/gnome/run_onchange_set_features.sh
@@ -9,7 +9,7 @@ gsettings set org.gnome.desktop.privacy remove-old-trash-files true
 gsettings set org.gnome.desktop.wm.preferences num-workspaces 4
 gsettings set org.gnome.mutter dynamic-workspaces false
 gsettings set org.gnome.mutter workspaces-only-on-primary false
-gsettings set org.gnome.shell favorite-apps "['org.gnome.nautilus.desktop', 'foot.desktop', 'org.telegram.desktop.desktop', 'obsidian.desktop']"
+gsettings set org.gnome.shell favorite-apps "['org.gnome.nautilus.desktop', 'org.telegram.desktop.desktop', 'obsidian.desktop']"
 gsettings set org.gnome.system.location enabled true
 
 # if ! lspci -nn | grep -i 'vga' | grep -iqE 'nvidia|geforce'; then
