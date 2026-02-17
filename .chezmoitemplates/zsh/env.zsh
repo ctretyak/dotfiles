d029a3d555e05d2f82cd000b796030434f7b8d21
ctretyak <ctretyak@gmail.com>
2026-02-17 08:35:56 +0200

    refactor: consolidate env vars into env.zsh with OS-specific templating

--- .chezmoitemplates/zsh/env.zsh
+++ .chezmoitemplates/zsh/env.zsh
@@ -1,7 +1,13 @@
 # ==============================================================================
 # Environment variables
 # ==============================================================================
+{{ if eq .chezmoi.os "linux" -}}
 export PATH=$HOME/bin:$HOME/.local/bin:/usr/local/bin:/snap/bin:$PATH
 export GPG_TTY=$TTY
+{{ else -}}
+export PATH=$HOME/bin:$HOME/.local/bin:/usr/local/bin:$PATH
+{{ end -}}
 export EDITOR='nvim'
+{{ if eq .chezmoi.os "linux" -}}
 export BROWSER='google-chrome-stable'
+{{ end -}}
