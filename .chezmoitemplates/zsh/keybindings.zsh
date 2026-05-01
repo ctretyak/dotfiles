5142349173cbbd307e6ed51e6d0770f172f92446
ctretyak <ctretyak@gmail.com>
2026-05-01 08:19:55 +0300

    zsh: drop redundant Ctrl+E autosuggest-accept binding

--- .chezmoitemplates/zsh/keybindings.zsh
+++ .chezmoitemplates/zsh/keybindings.zsh
@@ -1,6 +1,4 @@
 # ==============================================================================
 # Keybindings
 # ==============================================================================
-# Use Ctrl+E for autosuggest on all systems
-bindkey '^E' autosuggest-accept
 bindkey '^W' forward-word
